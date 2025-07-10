77ac46fe07d1b095cfe53d07c7e3a6b515fb66b8
david-irvinne <davidneilleen@gmail.com>
2025-07-10 18:16:50 +0700

    feat(waybar): add slider in pavucontrol

--- .config/waybar/scripts/bt-status.sh
+++ .config/waybar/scripts/bt-status.sh
@@ -7,10 +7,10 @@ if [[ $powered == "yes" ]]; then
     # Cek apakah ada device yang terhubung
     connected_device=$(bluetoothctl info | grep "Name:" | head -n1 | awk -F ': ' '{print $2}')
     if [[ -n "$connected_device" ]]; then
-        echo -e " $connected_device"
+        echo -e "󰂱 $connected_device"
     else
-        echo -e " ON"
+        echo -e "󰂯 オン"
     fi
 else
-    echo -e " OFF"
+    echo -e "󰂲 オフ"
 fi
