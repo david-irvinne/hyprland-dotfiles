084f8d729ecd5de225332d1bccfdda0c5194f557
david-irvinne <davidneilleen@gmail.com>
2025-08-24 11:31:22 +0700

    style(waybar): change edge radius

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,17 +1,28 @@
-return  {
+return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
   lazy = false, -- neo-tree will lazily load itself
   config = function()
     require("neo-tree").setup({
-      window = { width = 25}
+      window = { width = 25 },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {},
+          never_show = {
+            ".git",
+          },
+        },
+      },
     })
-    vim.keymap.set('n', '<c-n>', ':Neotree filesystem reveal left<cr>', {})
-  end
-} 
-
+    vim.keymap.set("n", "<c-n>", ":Neotree filesystem reveal left<cr>", {})
+  end,
+}
