abf9d2adc53031b8f68f43e405489ccf9ee41657
david-irvinne <davidneilleen@gmail.com>
2025-09-10 23:17:16 +0700

    feat(nvim shortcuts): add more shortcuts

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,9 +21,28 @@ require("lazy").setup("plugins")
 
 -- F5: Compile file C++ jadi binary, tanpa eksekusi
 vim.keymap.set("n", "<F5>", ":w<CR>:!g++ -std=c++20 -DDEBUG -Wall -Wshadow -Wextra % -o %:r<CR>", { noremap = true, silent = false })
+-- F6: Execute binary dari C++
+vim.keymap.set("n", "<F6>", ":w<CR>:terminal ./%:r<CR>", { noremap = true, silent = false })
+
+-- exit terminal job mode
+vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
 
 -- move between tabs
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>w', ':bdelete<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>t', ':tabnew<CR>', { noremap = true, silent = true })
+
+-- move between buffer 
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
+
+-- resize vertical split
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", { silent = true })
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", { silent = true })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { silent = true })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { silent = true })
+
+
