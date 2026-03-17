ffb1d090f9d0355e32988e196139dac6f00fdedc
david-irvinne <davidneilleen@gmail.com>
2026-03-18 00:37:19 +0700

    feat: wallpaper picker with rofi

--- .config/wallpaper/scripts/random-gif-wallpaper.sh
+++ .config/wallpaper/scripts/random-gif-wallpaper.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-DIR="$HOME/hypr-config/.config/wallpaper/gifs"
+DIR="$HOME/hypr-config/.config/wallpaper/"
 if [ ! -d "$DIR" ]; then
   notify-send "❌ Wallpaper Error" "Directory not found:\n$DIR" --urgency=critical --app-name="SWWW"
   exit 1
 fi
 
-FILE=$(find "$DIR" -type f -iname "*.gif" | shuf -n 1)
+FILE=$(find "$DIR" -type f -iname "*.*" | shuf -n 1)
 
 if [ -z "$FILE" ]; then
   notify-send "❌ No GIF Found" "No GIF file located in:\n$DIR" --urgency=critical --app-name="SWWW"
   exit 1
 fi
 
-TRANS="wipe"
+TRANS="random"
 RESIZE="crop"
 
 notify-send \
