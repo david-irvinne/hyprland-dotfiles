935269849f09cf52ac6488acf0ec84f4b90fcec0
david-irvinne <davidneilleen@gmail.com>
2026-07-16 16:58:15 +0700

    feat(nvim): allow filesearch inside dotfiles

--- .config/nvim/lua/plugins/explorer.lua
+++ .config/nvim/lua/plugins/explorer.lua
@@ -2,18 +2,25 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-      explorer = {
-        hidden = true,
-        ignored = true,
-        git = {
-          ignore = false,
-        },
-        exclude = {},
-      },
+      explorer = {},
       picker = {
         layout = {
           layout = {
             width = 25,
+          },
+        },
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+          grep = {
+            hidden = true,
+            ignored = true,
           },
         },
       },
