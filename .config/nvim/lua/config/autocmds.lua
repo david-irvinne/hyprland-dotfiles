93e65479e86657cec9292a446545efccbf7c9b00
david-irvinne <davidneilleen@gmail.com>
2026-08-29 22:31:12 +0700

    feat: autosave in nvim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -13,3 +13,49 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.b[args.buf].autoformat = false
   end,
 })
+
+-- Autosave file yang sedang diedit setelah berhenti mengetik sejenak.
+local autosave_group = vim.api.nvim_create_augroup("user_autosave", { clear = true })
+local autosave_timers = {}
+
+local function autosave(buf)
+  if not vim.api.nvim_buf_is_valid(buf) then
+    return
+  end
+
+  local should_save = vim.bo[buf].modified
+    and vim.bo[buf].buftype == ""
+    and vim.bo[buf].modifiable
+    and vim.bo[buf].readonly == false
+
+  if should_save then
+    vim.api.nvim_buf_call(buf, function()
+      vim.cmd("silent update")
+    end)
+  end
+end
+
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+  group = autosave_group,
+  callback = function(args)
+    local buf = args.buf
+    if autosave_timers[buf] then
+      autosave_timers[buf]:stop()
+      autosave_timers[buf]:close()
+    end
+
+    autosave_timers[buf] = vim.defer_fn(function()
+      autosave_timers[buf] = nil
+      autosave(buf)
+    end, 1000)
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FocusLost", "VimLeavePre" }, {
+  group = autosave_group,
+  callback = function()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      autosave(buf)
+    end
+  end,
+})
