ed24adbbda866ab3fe44e93b272918cfcafba9c9
david-irvinne <davidneilleen@gmail.com>
2026-08-30 13:48:42 +0700

    feat: envs for wayland

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,3 +7,16 @@ vim.keymap.set("n", "<leader>gg", function()
   vim.cmd("terminal lazygit")
   vim.cmd("startinsert")
 end)
+
+-- capslock for normal mode
+-- vim.api.nvim_create_autocmd({ "VimEnter", "FocusGained" }, {
+--   callback = function()
+--     os.execute("gsettings set org.gnome.desktop.input-sources xkb-options \"['caps:escape_shifted_capslock']\"")
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "VimLeave", "FocusLost" }, {
+--   callback = function()
+--     os.execute('gsettings set org.gnome.desktop.input-sources xkb-options "[]"')
+--   end,
+-- })
